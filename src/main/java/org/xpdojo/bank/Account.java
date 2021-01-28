22d93ff286347e03e4970ad00f61fd905e3106ce
Fei Ni <greenseven@gmail.com>
2021-01-28 10:43:27 -0500

    1st code change commit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,12 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+
+    public double balance(){
+        return balance;
+    }
+    public void deposit(int amount){
+        balance += amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
+
     @Test
 	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         Account account = new Account();
-        assertThat("my first test").isBlank();
+        account.deposit(100);
+        assertThat(account.balance()).isEqualTo(300);
     }
 }
